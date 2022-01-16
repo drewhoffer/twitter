06a99e43f7e263712de6e0ddf684c17ed98d1702
Fresh <drewsdhoffer@gmail.com>
2022-01-15 22:25:30 -0500

    Set static page as root route

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,5 @@ Rails.application.routes.draw do
   get "static_pages/help"
   get "static_pages/about"
   get "static_pages/contact"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get "/", to: "application#hello"  # Defines the root path route ("/")
-  # root "articles#index"
+  root "static_pages#home"
 end
