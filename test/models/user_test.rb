8d497bb8e8af5fa45a04f2250ec4375efef7feac
Fresh <drewsdhoffer@gmail.com>
2022-01-20 14:21:23 -0500

    Setup user with basic validation

---
+++ test/models/user_test.rb
@@ -0,0 +1,32 @@
+require "test_helper"
+
+class UserTest < ActiveSupport::TestCase
+  def setup
+    @user = User.new(name: "Example User", email: "user@example.com")
+  end
+
+  test 'should be valid' do
+    assert @user.valid?
+  end
+
+  test 'name should be present' do
+    @user.name = ''
+    assert_not @user.valid?
+  end
+
+  test 'email should be present' do
+    @user.email = ' '
+    assert_not @user.valid?
+  end
+  
+  test 'name not allowed name exceeding limit' do
+    @user.name = "a" * 51
+    assert_not @user.valid?
+  end
+
+  test 'email not allowed to exceed length' do
+    @user.email = "a" * 256
+    assert_not @user.valid?
+  end
+
+end
