2e8f2a8c07bc6c9d90b1aee4b75f2404ae4c3be0
Fresh <drewsdhoffer@gmail.com>
2022-01-21 23:19:23 -0500

    Create user signup form

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,9 @@
 module UsersHelper
+
+  # Returns the robohash for the given user.
+  def robohash_for(user, h: 100, w: 100)
+    robohash_id = Digest::MD5::hexdigest(user.email.downcase)
+    robohash_url = "https://robohash.org/#{robohash_id}.png?set=set1,3&bgset=bg2&size=#{h}x#{w}"
+    image_tag(robohash_url, alt: user.name, class: "robohash")
+  end
 end
